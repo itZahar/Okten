22cd3e33ddd9d273c2b8b7ffae55dcdf7e90750a
itZahar <zahargats1998@gmail.com>
2022-06-04 14:10:59 +0300

    lection2

---
+++ HomeWork/lection 2/main.js
@@ -0,0 +1,77 @@
+// let x = +prompt("what x?")
+// if (x !== 0){
+//     console.log("Вірно")
+// }
+// else {
+//     console.log('Не вірно!')
+// }
+
+// let time = +prompt ('what time?')
+// if (time <=0 &&time <= 15) {
+//     console.log('Перша чверть')
+// }
+//     else if (time > 15 && time <= 30){
+//     console.log('Друга чверть')
+// }
+//     else if (time > 30 && time <= 45){
+//     console.log('Третья чверть')
+// }
+//     else if(time > 45 && time <=59) {
+//     console.log('Четверта чверть')
+// }
+//     else {
+//     console.log('Не вірно')
+// }
+
+//     let day = prompt('what day is it today?')
+// if (typeof day === 'number') {
+//     if (day >= 1 && day <= 10) {
+//         console.log('перша')
+//     } else if (day >= 11 && day <= 20) {
+//         console.log('Друга')
+//     } else if (day >= 21 && day <= 31) {
+//         console.log('Третя')
+//     } else {
+//         console.log('???')
+//     }
+// } else {
+//     console.log('This is string')
+// }
+
+// switch (+prompt('what day is is today')) {
+//     case '1':
+//         document.write('Monday');
+//         break;
+//     case '2':
+//         document.write('Tuesday');
+//         break;
+//     case '3':
+//         document.write('Wednesday');
+//         break;
+//     case '4':
+//         document.write('Thursday');
+//         break;
+//     case '5':
+//         document.write('Friday');
+//         break;
+//     case '6':
+//         document.write('Saturday');
+//         break;
+//     case '7':
+//         document.write('Sunday');
+//         break;
+//     default:
+//         document.write('WTF')
+// }
+
+// let num = prompt('what is your number my friend?')
+// let num2 = prompt('What is your second number than?')
+// if (num > num2){
+//     document.write(num)
+// }
+//     else if (num2 > num){
+//         document.write(num2)
+// }
+//     else if (num === num2){
+//         document.write('They are the same')
+// }
